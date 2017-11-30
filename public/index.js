2679b442b3d32008ea117cde76e3fa6c787ea50e
Karan Atwal <karan.atwal@wns.com>
2017-11-30 16:06:34 +0530

    Add mustache for message templates

--- public/index.js
+++ public/index.js
@@ -8,11 +8,17 @@ socket.on('connect', () => {
 socket.on('newMessage', function (message) {
   console.log('New message: ', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery("#message-template").html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  jQuery("#messages").append(li);
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  //
+   jQuery("#messages").append(html);
 });
 
 socket.on('disconnect', () => {
@@ -21,15 +27,20 @@ socket.on('disconnect', () => {
 
 socket.on('geoLocationMessage', (message) => {
   var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery("#location-message-template").html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current geolocation</a>');
+  //
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current geolocation</a>');
-
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-
-  jQuery("#messages").append(li);
+  jQuery("#messages").append(html);
 });
 
 jQuery("#message-form").on('submit', function (e) {
