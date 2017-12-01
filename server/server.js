e24c6a084ae4aac9ca40caaeecbff6b1975b1741
Karan Atwal <karan.atwal@wns.com>
2017-12-01 18:27:35 +0530

    git ignore

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ io.on('connection', (socket) => {
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message, callback) => {
     console.log('Create message: ', message);
